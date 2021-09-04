01fd127543c997d4db96316319c399ccc1100b10
Filip <43730405+Gecazo@users.noreply.github.com>
2021-09-04 17:09:30 +0300

    Praca ze slownikiem odpowiedzi

--- python_repos.py
+++ python_repos.py
@@ -7,3 +7,11 @@ print(f"Kod stanu: {r.status_code}")
 response_dict = r.json()
 
 print(response_dict.keys())
+
+repo_dicts = response_dict['items']
+print(f"Liczba zwroconych repozytoriow: {len(repo_dicts)}")
+
+repo_dict = repo_dicts[0]
+print(f"\nKlucze: {len(repo_dict)}")
+for key in sorted(repo_dict.keys()):
+    print(key)
